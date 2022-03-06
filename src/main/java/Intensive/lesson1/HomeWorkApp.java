efc3fc2fc4449e879b6fda0925f2433eddaa16fa
Olbog <olbog2009@bk.ru>
2022-03-06 16:45:45 +0300

    Task 1

---
+++ src/main/java/Intensive/lesson1/HomeWorkApp.java
@@ -0,0 +1,12 @@
+package Intensive.lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        // 55
+    }
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
