29278816dd13911604ecaae294bedc2d677e16d8
Olbog <olbog2009@bk.ru>
2022-03-06 20:01:53 +0300

    Тренировка

--- src/main/java/Intensive/lesson1/MainApp.java
+++ src/main/java/Intensive/lesson1/MainApp.java
@@ -1,7 +1,6 @@
 package Intensive.lesson1;
 
 public class MainApp {
-    public static void main(String[] args) {
-        System.out.println("1");
+    public static void main(String[] args){
     }
 }
