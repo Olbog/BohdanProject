fe49b1b20b537bc1f1e949a16907a9ba60288573
Olbog <olbog2009@bk.ru>
2022-03-07 18:06:15 +0300

    1

--- src/main/java/Work/lesson1/HomeWork1.java
+++ src/main/java/Work/lesson1/HomeWork1.java
@@ -1,10 +1,41 @@
 package Work.lesson1;
 
+import java.util.Arrays;
+
 public class HomeWork1 {
     public static void main(String[] args) {
-        mas();
+        int[]mas2 = new int [] {5,6,3,8,4,5,6,1,9,4};
+        quickSort(mas2, 0, mas2.length - 1);
     }
-    public static void mas(){
+    public static void quickSort(int[] mas, int leftBord, int rightBord ){
+        int leftMark = leftBord;
+        int rightMark = rightBord;
+        int pivot = mas[(leftMark + rightMark) /2];
+        do{
+            while (mas[leftMark] < pivot){
+                leftMark ++;
+            }
+            while (mas[rightMark] > pivot){
+                rightMark ++;
+            }
+            if(leftMark >= rightMark){
+                if(leftMark > rightMark){
+                    int b = leftMark;
+                    leftMark = rightMark;
+                    rightMark = b;
+                }
+                leftMark ++;
+                rightMark --;
+            }
+        } while (leftMark > rightMark);
+        if (leftMark > rightMark){
+            quickSort(mas, leftMark, rightBord);
+        }
+        if (leftBord > rightMark){
+            quickSort(mas, leftBord, rightMark);
+        }
+        for (int i = 0; 0 < mas.length; i++)
+            System.out.println(mas[i]);
 
     }
 }
