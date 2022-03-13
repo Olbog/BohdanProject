c27297b249d3e041b91d7933588005c6e75a9ceb
Olbog <olbog2009@bk.ru>
2022-03-13 16:52:59 +0300

    123

---
+++ src/main/java/Intensive/lesson3/Employee.java
@@ -0,0 +1,60 @@
+package Intensive.lesson3;
+
+import java.util.SplittableRandom;
+
+public class Employee {
+    public static void main(String[] args) {
+        EmployeePerson employeePerson1 = new EmployeePerson("Ivanov Ivan", "Engineer", "lol2@mail.ru", 6743453, 3000, 35);
+
+        EmployeePerson[] mas = new EmployeePerson[5];
+        mas[0] = new EmployeePerson("Ivanov Ivan","Engineer","ivanov@mail.ru", 54373465, 3000, 29 );
+        mas[1] = new EmployeePerson("Petrov Petr", "Engineer2", "petrov@mail.ru", 58567546, 3100, 32);
+        mas[2] = new EmployeePerson("Antonov Anton", "Engineer3", "antonov@mail.ru", 58567146, 3200, 37);
+        mas[3] = new EmployeePerson("Sidorov Mark", "Engineer4", "sidorov@mail.ru", 53287146, 3350, 41);
+        mas[4] = new EmployeePerson("Izumin Idrak", "Engineer5", "izumin@mail.ru", 57836146, 3500, 43);
+
+        for(int a=0; a < mas.length; a++ ){
+            if (mas[a].age >=40){
+                System.out.println(mas[a]);
+                System.out.println("\n");
+            }
+        }
+
+
+    }
+
+}
+class EmployeePerson {
+    String fio;
+    String post;
+    String email;
+    int phone;
+    int salary;
+    int age;
+
+
+
+    public EmployeePerson(String fio,String post, String email, int phone, int salary, int age) {
+        this.fio = fio;
+        this.post = post;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+
+    @Override
+    public String toString(){
+        return (fio + "\n"
+                + post + "\n"
+                + email + "\n"
+                + phone + "\n"
+                + salary + "\n"
+                + age);
+    }
+
+}
+
+
+
