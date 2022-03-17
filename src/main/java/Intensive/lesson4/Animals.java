b4d154427183b2c2866dbad59588375e62d11b36
Olbog <olbog2009@bk.ru>
2022-03-17 15:11:15 +0300

    Коты и собаки (4)

--- src/main/java/Intensive/lesson4/Animals.java
+++ src/main/java/Intensive/lesson4/Animals.java
@@ -3,11 +3,18 @@ package Intensive.lesson4;
 public class Animals {
     public static void main(String[] args) {
         Cats cat1 = new Cats("Jon");
+        count1++;
         Dogs dog1 = new Dogs("Kim");
+        count2++;
         dog1.swim(5);
+        System.out.println("Общее кол-во котов: " + count1);
+        System.out.println("Общее кол-во собак: " + count2);
+        System.out.println("Общее кол-во животных: " + (count1+count2));
     }
 
     String name;
+    static int count1 = 0;
+    static int count2 = 0;
 
 
     Animals(String name){
